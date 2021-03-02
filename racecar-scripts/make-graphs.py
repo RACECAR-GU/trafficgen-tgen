4a715d54b37c9bd3c35384ee34073434e2cb443f
Micah Sherr <msherr@cs.georgetown.edu>
2021-03-01 22:14:57 -0500

    more work, but still confused

--- racecar-scripts/make-graphs.py
+++ racecar-scripts/make-graphs.py
@@ -39,62 +39,85 @@ def silent_mkdir( dir ):
         pass
     
 
+def generate_packetflow_markov_model( args, protocol_number ):
+    logging.info( 'creating a new packetflow Markov Model; protocol number %d' % protocol_number )
+    G = nx.DiGraph()
 
-def generate_protocol( args, proto_num ):
+    switch_bias_sending = random.uniform(0.01,0.3)
+    switch_bias_receiving = random.uniform(0.01,0.3)
 
-    # TODO: everything hard-coded should really be a configurable option
-    num_streams = random.randint(1,5)          # TODO
-    G_client = nx.DiGraph()
-    G_client.add_node("start", time="1 second", heartbeat="1 second", loglevel="message", peers=args.peer)
-    G_client.add_node("end", time="1 minute", count="%s" % num_streams, recvsize="1 GiB", sendsize="1 GiB")
+    logging.info( f'send/recv switch biases are {switch_bias_sending} and {switch_bias_receiving}')
 
-    for stream_id in range(num_streams):
-        G_client.add_node("stream%d" % stream_id,
-            sendsize="%d kib" % random.randint(1,10),
-            recvsize="%d kib" % random.randint(1,100),
-        )
-        G_client.add_node(
-            "pause%d" % stream_id,
-            time="%d ms" % random.randint(0,1000)
-        )
+    G.add_node('s0', type="state", name='start')
+    G.add_node('s1', type="state", name='clientsend')
+    G.add_node('s2', type="state", name='serversend')
+    G.add_node('s3', type="state", name='devnull')
+
+    G.add_node('o1', type="observation", name='+')  # client to server
+    G.add_node('o2', type="observation", name='-')  # server to client
+    G.add_node('o3', type="observation", name='F')  # stop sending
+
+    # we start by either having the client or server send
+    G.add_edge('s0', 's1', type='transition', weight=1.0)
+    G.add_edge('s0', 's2', type='transition', weight=1.0)
+
+    # moving between send and receive states is biased in favor of not switching
+    G.add_edge('s1', 's2', type='transition', weight=switch_bias_sending)
+    G.add_edge('s1', 's1', type='transition', weight=(1-switch_bias_sending))   # don't switch
+    
+    G.add_edge('s2', 's1', type='transition', weight=switch_bias_receiving)
+    G.add_edge('s2', 's2', type='transition', weight=(1-switch_bias_receiving))   # don't switch
+
+    # define some emissions for sending and receiving states
+    # TODO: random the delays; remove hardcoded stuff
+    G.add_edge('s1', 'o1', type='emission', weight=1.0, distribution='exponential', param_rate=100.0)
+    G.add_edge('s2', 'o2', type='emission', weight=1.0, distribution='exponential', param_rate=100.0)
+    
+    # add some probability of ending
+    #G.add_edge('s1', 'o3', type='emission', weight=0.01, distribution='uniform', param_low=50, param_high=500)
+    #G.add_edge('s2', 'o3', type='emission', weight=0.03, distribution='uniform', param_low=50, param_high=500)
+    
+    #G.add_edge('s1', 's3', type='transition', weight=0.0001)
+    #G.add_edge('s2', 's3', type='transition', weight=0.0001)
+    #G.add_edge('s3', 's3', type='transition', weight=1.0)
+    #G.add_edge('s3', 'o3', type='emission', weight=1.0, distribution='uniform', param_low=50, param_high=500)
+
+    nx.write_graphml(G, f"{args.outputdir}/protocol{protocol_number}.tgenrc.graphml" )
 
 
-    # connect the graph
-    prev_stream = "start"
-    for stream_id in range(num_streams):
-        G_client.add_edge( 
-            prev_stream,
-             "pause%d" % stream_id
-        )
-        G_client.add_edge( 
-             "pause%d" % stream_id,
-             "stream%d" % stream_id
-        )
-        prev_stream = "stream%d" % stream_id
-    G_client.add_edge( 
-        "stream%d" % (num_streams - 1),
-        "end")
 
-    nx.write_graphml( 
-        G_client, 
-        "%s/proto-%d-client-stream.tgenrc.graphml" % (args.outputdir, proto_num) 
-    )
+def generate_client_protocol( args, number_of_protocols ):
+    logging.info( 'creating the client, with %d protocols' % number_of_protocols )
+    G = nx.DiGraph()
+
+    G.add_node("start", time="1 second", heartbeat="1 second", loglevel="message", peers=args.peer)
+    G.add_node("pause", time="5 seconds")
+    for i in range(number_of_protocols):
+        generate_packetflow_markov_model( args, i )
+        G.add_node(f"stream{i}", packetmodelpath=f"{args.outputdir}/protocol{i}.tgenrc.graphml", packetmodelmode="graphml", timeout="10 minutes", stallout="5 minutes")
+    
+    G.add_edge("start", "pause")
+    for i in range(number_of_protocols):
+        G.add_edge("pause", f"stream{i}", weight="1.0")     # TODO: wouldn't there be a large outdegree from single pause node?
+        G.add_edge(f"stream{i}", "pause")
+
+    nx.write_graphml(G, "%s/client-unclass.tgenrc.graphml" % args.outputdir)
 
 
 
-def run_tgen( args, proto_num ):
+def run_tgen( args ):
     for run in range(args.runs):
         logging.debug( "starting pcap")
         proc = subprocess.Popen( [
             '/usr/sbin/tcpdump',
             '-n', 
             '-i', args.cap_interface,
-            '-w', '%s/capture-proto-%d-run-%d.pcap' % (args.capdir,proto_num,run)
+            '-w', '%s/capture-proto-run-%d.pcap' % (args.capdir,run)
             ] )
         logging.debug( "starting tgen" )
         os.system( "%s %s" % (
             args.tgen,
-            os.path.abspath("%s/proto-%d-client-stream.tgenrc.graphml" % (args.outputdir,proto_num))
+            os.path.abspath( f"{args.outputdir}/client-unclass.tgenrc.graphml" )
         ))
         logging.debug("stopping tgen and waiting 2 seconds")
         time.sleep(1.5)
@@ -123,17 +146,12 @@ def main():
         "%s/proto-server-stream.tgenrc.graphml" % args.outputdir 
     )
 
-
-
-    for i in range(args.number_of_protocols):
-        logging.info( 'creating protocol number %d', i )
-        generate_protocol( args, i )
+    generate_client_protocol( args, args.number_of_protocols )
 
     if args.run_tgen is True:
         logging.warning( "you should run the noroot_tcpdump.sh script to ensure this user can actually run tcpdump")
-        for i in range(args.number_of_protocols):
-            logging.info( 'running tgen for protocol %d' % i )
-            run_tgen( args, i )
+        logging.info( 'running tgen' )
+        run_tgen( args )
 
     return 0        # all's well that ends well
 
